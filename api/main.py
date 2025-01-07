b424c3621a8063ba3f1e20ad12f2807c227e14f1
Alvpvtos <66924686+alvpvtos@users.noreply.github.com>
2025-01-06 22:07:43 -0600

    working on api

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
-
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Body 
+from pydantic import BaseModel, Field
 from db.operations import (
     create_new_product, 
     search_product_by_product_id, 
@@ -18,10 +18,30 @@ from db.operations import (
 
 app = FastAPI()
 
+############### Models   ###############
+class Product(BaseModel):
+    name:str = Field(..., example="Garden Hose", description="The name of your product")
+    description:str = Field(..., example="This is a garden hose, used to water your plants", description="The description of your product")
+    additional_product_ids: None | list[dict] = Field(default=None, description="A list of additional product IDs for your product, " )
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Flexzilla HFZG550YW Garden Lead-In Hose 5/8 In. x 50 ft, Heavy Duty, Lightweight, Drinking Water Safe ",
+                    "description": "Flexzilla Garden Hose is engineered with a Flexible Hybrid Polymer that is both lightweight and durable. This premium hose redefines flexibility and is easy to maneuver around your yard. Flexzilla Garden Hose has zero memory which allows the hose to lay flat and ensures that sprinklers stay put without twisting.",
+                    "additional_product_ids": [{"UPC":"092329591512","ASIN":"B003TFE7ZM"}]
+                }
+            ]
+        }
+    }
+
+
 
 ############### Products   ###############
-@app.post("/products/{product_identifier}")
-def create_new_product(product: Product):
-    pass
+#  create a new product
+@app.post("/products/new")
+def create_new_product(product: Annotated[Product,Body()]) -> str:
+    return product.name
+
 ############### Shelves    ###############
 ############### Containers ###############
